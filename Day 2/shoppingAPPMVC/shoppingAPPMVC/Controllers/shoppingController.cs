08752df84ba73696c1473d6cf8d6eb969bfe699c
Nikhil <nikhilshah.net@hotmail.com>
2023-01-19 10:42:41 +0530

    Day 2 - MVC First App

---
+++ Day 2/shoppingAPPMVC/shoppingAPPMVC/Controllers/shoppingController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace shoppingAPPMVC.Controllers
+{
+    public class shoppingController : Controller
+    {
+
+        public IActionResult Products()
+        {
+            //collect data from model, pass it in a Bag - viewbag            
+            return View();
+        }
+
+        public IActionResult Client()
+        {
+            return View();
+        }
+
+        public IActionResult Orders()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+      
+    }
+}
