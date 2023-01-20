8dd9451dcf66e3565a34f501b01d6c88244f1614
Nikhil <nikhilshah.net@hotmail.com>
2023-01-20 10:40:13 +0530

    Day 3 - MVC Communication

---
+++ Day 3/shoppingAPPMVC_2/shoppingAPPMVC_2/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shoppingAPPMVC_2.Models;
+namespace shoppingAPPMVC_2.Controllers
+{
+    public class ProductsController : Controller
+    {
+     
+
+
+        public IActionResult ProductsHome()
+        {
+            //collect data from Model
+
+            //this data would come from Model, model will get it from somewhere
+            ViewBag.totalProducts = 50;
+            ViewBag.developer = "Nikhil Shah";
+            ViewBag.isProjectLive = false;
+
+            List<string> topSellingProducts = new List<string>()
+            {
+                "IPhone","Superdry","Puma","Nike","Pepsi","Maggie","Macbook Air"
+            };
+
+            ViewBag.topProducts = topSellingProducts;
+
+
+            return View();
+        }
+
+
+        ProductDetailsModel pObj = new ProductDetailsModel();
+        public IActionResult ProductDetails()
+        {
+            ViewBag.pList = pObj.GetAllProducts();
+            return View();
+        }
+
+
+    }
+}
---
+++ Day 3/shoppingAPPMVC_2/shoppingAPPMVC_2/Models/ProductDetailsModel.cs
@@ -0,0 +1,34 @@
+﻿namespace shoppingAPPMVC_2.Models
+{
+    public class ProductDetailsModel
+    {
+        #region Properties
+        public int pId { get; set; }
+        public string pName { get; set; }
+        public string pCategory { get; set; }
+        public double pPrice { get; set; }
+        public bool pIsInStock { get; set; }
+        #endregion
+
+
+        static List<ProductDetailsModel> pList = new List<ProductDetailsModel>()
+        {
+            new ProductDetailsModel(){ pId=101, pName="Iphone", pCategory="Electronics", pPrice=140000, pIsInStock=true },
+            new ProductDetailsModel(){ pId=102, pName="Dell lattitude", pCategory="Electronics", pPrice=140000, pIsInStock=true },
+            new ProductDetailsModel(){ pId=103, pName="Pepsi", pCategory="Cold-Drink", pPrice=140000, pIsInStock=false },
+            new ProductDetailsModel(){ pId=104, pName="Nike", pCategory="Shoes", pPrice=140000, pIsInStock=true },
+            new ProductDetailsModel(){ pId=105, pName="Puma", pCategory="Shoes", pPrice=140000, pIsInStock=true },
+            new ProductDetailsModel(){ pId=106, pName="Air pods", pCategory="Electronics", pPrice=140000, pIsInStock=true },
+            new ProductDetailsModel(){ pId=107, pName="Fossil Q", pCategory="Electronics", pPrice=140000, pIsInStock=false },
+            new ProductDetailsModel(){ pId=108, pName="Appy Fizz", pCategory="Cold-Drink", pPrice=140000, pIsInStock=true },
+            new ProductDetailsModel(){ pId=109, pName="Maggie", pCategory="Junk-Food", pPrice=140000, pIsInStock=false },
+        };
+
+        public List<ProductDetailsModel> GetAllProducts()
+        { 
+            return pList;
+        }
+
+
+    }
+}
