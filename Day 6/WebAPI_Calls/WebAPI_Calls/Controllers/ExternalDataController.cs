8344bdbde3d24822fa8747dd57869730d30cd126
Nikhil <nikhilshah.net@hotmail.com>
2023-01-25 10:48:13 +0530

    WebAPI Call

---
+++ Day 6/WebAPI_Calls/WebAPI_Calls/Controllers/ExternalDataController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_Calls.Controllers
+{
+    public class ExternalDataController : Controller
+    {
+      
+        public IActionResult PostData()
+        {
+            return View();
+        }
+
+        public IActionResult PhotosData()
+        {
+            return View();
+        }
+    }
+}
---
+++ Day 6/WebAPI_Calls/WebAPI_Calls/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAPI_Calls.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
